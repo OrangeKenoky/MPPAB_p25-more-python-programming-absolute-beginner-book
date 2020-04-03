b5d636aef21055688d5801f6923b5ba6aca5ad5a
OrangeKenoky <62431309+OrangeKenoky@users.noreply.github.com>
2020-04-03 11:06:30 -0400

    Create MPPAB_p25.py

---
+++ MPPAB_p25.py
@@ -0,0 +1,22 @@
+#--------------
+import pygame
+from pygame.locals import *
+pygame.init()
+#--------------
+screen = pygame.display.set_mode((600,500))
+myfont=pygame.font.Font(None, 60)
+white = 255,255,255
+blue= 0,0,255
+#--------------
+textImage = myfont.render("Hello Pygame", True, white)
+#--------------
+screen.fill(blue)
+screen.blit(textImage, (100,100))
+pygame.display.update()
+    
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
